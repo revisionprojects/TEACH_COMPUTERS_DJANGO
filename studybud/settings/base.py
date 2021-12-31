bb8a4bcc51cdf6e2e8ed0d3a2fe1174260b46e34
revisionprojects <robinmatth@live.ca>
2021-12-31 11:24:12 -0500

    updated settings template directory

--- studybud/settings/base.py
+++ studybud/settings/base.py
@@ -43,7 +43,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR / 'templates'
+            BASE_DIR / 'studybud/templates'
         ],
         'APP_DIRS': True,
         'OPTIONS': {
