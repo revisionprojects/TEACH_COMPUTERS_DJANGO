a5ddccb245bae3a28046f0cd707aef54bf22ff78
revisionprojects <robinmatth@live.ca>
2021-12-31 00:54:21 -0500

    added staticfile storage link

--- studybud/settings.py
+++ studybud/settings.py
@@ -136,7 +136,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-
+STATICFILES_STORAGE =  'django.contrib.staticfiles.storage.StaticFilesStorage' 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
