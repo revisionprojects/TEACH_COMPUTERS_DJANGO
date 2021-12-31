e112b2141b076e26da00d14b3b9f39b3e47a7789
revisionprojects <robinmatth@live.ca>
2021-12-31 00:11:35 -0500

    add postgres for heroku support

--- studybud/settings.py
+++ studybud/settings.py
@@ -87,12 +87,20 @@ WSGI_APPLICATION = 'studybud.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+  #      'NAME': BASE_DIR / 'db.sqlite3',
+ #   }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        
+        'CONN_MAX_AGE': 500
+            }
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 
 # Password validation
