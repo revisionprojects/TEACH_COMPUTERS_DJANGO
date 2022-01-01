f957b9dc47d40283f3b690ddc4f3ff09820605c4
revisionprojects <robinmatth@live.ca>
2022-01-01 01:28:19 -0500

    updating prod.py

--- studybud/settings/production.py
+++ studybud/settings/production.py
@@ -3,7 +3,7 @@ from .base import *
 import django_heroku
 import dj_database_url,os
 
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ['SECRET_KEY']
 DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
